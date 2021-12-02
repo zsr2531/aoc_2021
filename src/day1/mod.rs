3018a19265434c5df36b5ea0a2971bb63dc61e65
zsr2531 <warhaamer1337@gmail.com>
2021-12-02 11:38:50 +0100

    Factor out duplicate code in day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -2,32 +2,25 @@ use crate::common::*;
 
 pub struct Day1;
 
+fn solve(input: &str, offset: usize) -> usize {
+    let lines: Vec<i32> = input
+        .lines()
+        .map(|l| l.parse::<i32>().unwrap())
+        .collect();
+        
+    lines
+        .iter()
+        .zip(lines.iter().skip(offset))
+        .filter(|(x, y)| x < y)
+        .count()
+}
+
 impl Solver for Day1 {
     fn part1(&self, input: &str) -> Solution {
-        let lines: Vec<i32> = input
-            .lines()
-            .map(|l| l.parse::<i32>().unwrap())
-            .collect();
-
-        lines
-            .iter()
-            .zip(lines.iter().skip(1))
-            .filter(|(x, y)| x < y)
-            .count()
-            .into()
+        solve(input, 1).into()
     }
 
     fn part2(&self, input: &str) -> Solution {
-        let lines: Vec<i32> = input
-            .lines()
-            .map(|l| l.parse::<i32>().unwrap())
-            .collect();
-            
-        lines
-            .iter()
-            .zip(lines.iter().skip(3))
-            .filter(|(x, y)| x < y)
-            .count()
-            .into()
+        solve(input, 3).into()
     }
 }
