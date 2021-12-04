6e10048e60bbd5a65a5ff49e903c1eb3170d087a
zsr2531 <warhaamer1337@gmail.com>
2021-12-04 14:49:49 +0100

    Remove old day 1 code

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -31,28 +31,3 @@ impl ParseInput<Day1> for AdventOfCode2021 {
             .collect()
     }
 }
-
-// pub struct Day1;
-
-// fn solve(input: &str, offset: usize) -> usize {
-//     let lines: Vec<i32> = input
-//         .lines()
-//         .map(|l| l.parse::<i32>().unwrap())
-//         .collect();
-        
-//     lines
-//         .iter()
-//         .zip(lines.iter().skip(offset))
-//         .filter(|(x, y)| x < y)
-//         .count()
-// }
-
-// impl Solver for Day1 {
-//     fn part1(&self, input: &str) -> Solution {
-//         solve(input, 1).into()
-//     }
-
-//     fn part2(&self, input: &str) -> Solution {
-//         solve(input, 3).into()
-//     }
-// }
